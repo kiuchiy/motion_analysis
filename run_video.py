7862ad78273e22d45d2ade4e6913650d8f6210c2
ryutaro <ryu.6171@gmail.com>
2019-04-14 13:14:10 +0900

    DEBUG polydotline

--- run_video.py
+++ run_video.py
@@ -154,8 +154,8 @@ def run_video(video, path='', skip_cog=False, skip_track=False, cog_color='black
                 df_human = track.humans_tracklet[track.humans_tracklet[:, track.clm_num] == hum]
                 df_human = df_human.astype(int)
                 trajectories = np.array([(gdf[4 * 3 + 1], gdf[4 * 3 + 2]) for gdf in df_human if gdf[4 * 3 + 1]])
-                # cv2.polylines(img, [trajectories], False, (200, 200, int(i%3)*30), 3, cv2.LINE_4)
-                polydotline(img, [trajectories], (200, 200, int(i%3)*30))
+                cv2.polylines(img, [trajectories], False, (200, 200, int(i%3)*30), 3, cv2.LINE_4)
+                # polydotline(img, [trajectories], (200, 200, int(i%3)*30))
                 trajectories = np.array([(gdf[7 * 3 + 1], gdf[7 * 3 + 2]) for gdf in df_human if gdf[7 * 3 + 1]])
                 cv2.polylines(img, [trajectories], False, (int(i%3)*30, 200, int(i%3)*30), 3, cv2.LINE_4)
                 # polydotline(img, [trajectories], (200, 200, int(i%3)*30))
